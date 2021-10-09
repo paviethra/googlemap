cc0f11ece21fb3f8610bb11957b243072fcbe2cb
paviethra <62457057+paviethra@users.noreply.github.com>
2021-10-09 17:43:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// function initMap(){
+//     var options ={
+//         center: {lat:20.5937,lng:78.9629},
+//         zoom: 1
+//     }
+//     var map = google.maps.Map(document.getElementById('map'),options)
+// }
